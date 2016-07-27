2f66e2a49d0e824b4dbb34a49e11e81ee16946a6
William Anderson <williamharrisanderson@gmail.com>
2016-07-26 17:52:45 -0700

    Initiate Flask output

--- FlaskOutput.py
+++ FlaskOutput.py
@@ -1,7 +1,18 @@
+from __future__ import division
+from __future__ import print_function
+import warnings
+
 from flask import Flask
-app = Flask(__name__)
+import os.path
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+APP = Flask(__name__.split('.')[0])
 
+
+@APP.route("/")
+def home_page():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    # Run on port 8003, allow connections from all IP addresses
+    APP.run(port=8003, host='0.0.0.0', debug=True)
